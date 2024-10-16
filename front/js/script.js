350efd66d083a2dd9358c750149aa03f2e239a13
andres <2024_andres.suarez@iticbcn.cat>
2024-10-16 15:09:01 +0200

    JS modificado

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Cridem a l'endpoint de l'API fent un fetch
     ______________________________________________
+    fetch ('http://127.0.0.1:8000/alumne/listAll')
         .then(response => {
             if (!response.ok) {
                 throw new Error("Error a la resposta del servidor");
@@ -14,16 +15,34 @@ document.addEventListener("DOMContentLoaded", function() {
             // Iterar sobre los alumnos y agregarlos al DOM
             data.forEach(alumne => {
                 const row = document.createElement("tr");
-
+                
+                // Afegim les altres cel·les amb les dades de l'alumne
                 const nomAluCell = document.createElement("td");
                 nomAluCell.textContent = alumne.NomAlumne;
                 row.appendChild(nomAluCell);
 
-                // Repetir per tots els altres camps restants que retorna l'endpoint
+                // Afegim la cel·la pel cicle                
+                const cicleCell = document.createElement("td");
+                cicleCell.textContent = alumne.Cicle;
+                row.appendChild(cicleCell);
+
+                // Afegim la cel·la pel curs
+                const cursCell = document.createElement("td");
+                cursCell.textContent = alumne.Curs;
+                row.appendChild(cursCell);
+
+                // Afegim la cel·la pel grup
+                const grupCell = document.createElement("td");
+                grupCell.textContent = alumne.Grup;
+                row.appendChild(grupCell);
+
+                // Afegim la cel·la pel nom de l'aula (DescAula)
+                const aulaCell = document.createElement("td");
+                aulaCell.textContent = alumne.DescAula;
+                row.appendChild(aulaCell);
                 _____________________________________________
 
-                
-
+                // Afegir la fila completa a la taula
                 alumnesTableBody.appendChild(row);
             });
         })
